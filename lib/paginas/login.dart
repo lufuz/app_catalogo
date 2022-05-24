906f2b4df4143505a4649820cc5558c3add85eba
lufuz <luxangelax@gmail.com>
2022-05-24 17:08:03 -0500

    se agregaron elementos a la pantalla de login

--- lib/paginas/login.dart
+++ lib/paginas/login.dart
@@ -4,10 +4,55 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
+        color: Colors.white,
         child: Column(
-      children: [
-        Image.asset("assets/images/login_image.png"),
-      ],
-    ));
+          children: [
+            Image.asset(
+              "assets/images/green_login.png",
+              fit: BoxFit.cover,
+            ),
+            Text(
+              "Inicio de sesión",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Ingrese su correo electrónico",
+                      labelText: "Correo electrónico",
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Ingrese su correo contraseña",
+                      labelText: "Contraseña",
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  ElevatedButton(
+                    child: Text("Iniciar sesión"),
+                    style: TextButton.styleFrom(),
+                    onPressed: () {
+                      print("¡Bienvenid@!");
+                    },
+                  )
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
