302e307bb08fd0a7f67ffd5d96263a266c0dc26b
Alan25-09 <alanfloresloyola@gmail.com>
2022-05-26 00:01:42 -0500

    Hice la animación del botón

--- lib/paginas/login.dart
+++ lib/paginas/login.dart
@@ -1,8 +1,15 @@
 import 'package:app_catalogo/utils/routes.dart';
 import 'package:flutter/material.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
   @override
+  _LoginState createState() => _LoginState();
+  }
+
+  class _LoginState extends State<Login>{
+    bool changeButton= false;
+
+    @override
   Widget build(BuildContext context) {
     return Material(
         color: Colors.white,
@@ -11,8 +18,7 @@ class Login extends StatelessWidget {
           children: [
             Image.asset(
               "assets/images/green_login.png",
-              fit: BoxFit.cover,
-              
+              fit: BoxFit.cover,             
             ),
             Text(
               "Inicio de sesión",
@@ -45,13 +51,43 @@ class Login extends StatelessWidget {
                   SizedBox(
                     height: 20.0,
                   ),
-                  ElevatedButton(
-                    child: Text("Iniciar sesión"),
-                    style: TextButton.styleFrom(minimumSize:Size(150, 40)),
-                    onPressed: () {
-                      Navigator.pushNamed(context, MyRoutes.homeRoute);
+                  InkWell(
+                    onTap:(){
+                      setState(() {
+                        changeButton=true;
+                      });
+                      // Navigator.pushNamed(context, MyRoutes.homeRoute);
                     },
-                  )
+                    child:AnimatedContainer(
+                      duration: Duration(seconds: 1),
+                      width: changeButton? 50: 150,
+                      height: 50,
+                      alignment: Alignment.center,
+                      child: changeButton
+                      ?Icon(
+                        Icons.done,
+                        color:Colors.white,
+                        )
+                       :Text(
+                      "Login",
+                      style:TextStyle(color: Colors.white, 
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18),
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.green,
+                        borderRadius: 
+                        BorderRadius.circular(changeButton? 50: 8),
+                      ),
+                    ),
+                  ),
+                  // ElevatedButton(
+                  //   child: Text("Iniciar sesión"),
+                  //   style: TextButton.styleFrom(minimumSize:Size(150, 40)),
+                  //   onPressed: () {
+                  //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                  //   },
+                  // )
                 ],
               ),
             )
