8d7b97fb0520cc3d053f7a25749a78c6e79c93a8
lufuz <luxangelax@gmail.com>
2022-05-28 15:56:38 -0500

    creacion del catalogo de productos

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: MyTheme.lightTheme(context),
       darkTheme: MyTheme.darkTheme(context),
       debugShowCheckedModeBanner: false,
-      initialRoute: "/home",
+      initialRoute: "/login",
       routes: {
         "/": (context) => Login(),
         MyRoutes.homeRoute: (context) => HomePage(),
--- lib/paginas/pagina_inicio.dart
+++ lib/paginas/pagina_inicio.dart
@@ -1,11 +1,15 @@
+import 'package:app_catalogo/models/catalogo.dart';
 import 'package:flutter/material.dart';
 import '../widgets/drawer.dart';
+import '../widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   final int days = 30;
   final String name = "Green House";
   @override
   Widget build(BuildContext context) {
+    //Lista para poder ejemplificar como se verian todos los productos
+    final lista = List.generate(5, (index) => CatalogoModelo.items[0]);
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -16,9 +20,15 @@ class HomePage extends StatelessWidget {
           ),
         ),
       ),
-      body: Center(
-        child: Container(
-          child: Text("Bienvenido la pagina $days de Flutter para $name"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: lista.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: lista[index],
+            );
+          },
         ),
       ),
       drawer: MyDrawer(),
