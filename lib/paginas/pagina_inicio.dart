e26615b01268a8d3652e8bdab1011874747505eb
lufuz <luxangelax@gmail.com>
2022-06-03 20:14:08 -0500

    Creacion de archivo json para los productos

--- lib/paginas/pagina_inicio.dart
+++ lib/paginas/pagina_inicio.dart
@@ -1,11 +1,33 @@
 import 'package:app_catalogo/models/catalogo.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import '../widgets/drawer.dart';
 import '../widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final int days = 30;
+
   final String name = "Green House";
+
+  @override
+  void initState() {
+    super.initState();
+    cargarProductos();
+  }
+
+  cargarProductos() async {
+    var catalogojson =
+        await rootBundle.loadString("assets/files/catalogo.json");
+    var infoDecodificada = jsonDecode(catalogojson);
+    print(infoDecodificada);
+  }
+
   @override
   Widget build(BuildContext context) {
     //Lista para poder ejemplificar como se verian todos los productos
