f218d3b362ef357482932dbe3f308c848de43e8e
lufuz <luxangelax@gmail.com>
2022-06-05 15:27:12 -0500

    modificacion al diseño del listado de los productos

--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+/*Clase que permite acceder a las propiedades de los temas para la aplicacion,
+contienen los colores, fuentes e íconos necesarios */
+
 class MyTheme {
   static ThemeData lightTheme(BuildContext context) => ThemeData(
       primarySwatch: Colors.lightGreen,
-      primaryTextTheme: GoogleFonts.robotoTextTheme(),
+      primaryTextTheme: GoogleFonts.latoTextTheme(),
       appBarTheme: AppBarTheme(
         color: Colors.lightGreen,
         elevation: 0.0,
@@ -15,4 +18,11 @@ class MyTheme {
   static ThemeData darkTheme(BuildContext context) => ThemeData(
         brightness: Brightness.dark,
       );
+
+  /*Colores a utilizar */
+
+  static Color verdeTitulo = Colors.lightGreen.shade900;
+  static Color verdeSubtitulo = Colors.lightGreen.shade800;
+  static Color colorFondo = Colors.green.shade50;
+  static Color colorTarjeta = Colors.green.shade100;
 }
